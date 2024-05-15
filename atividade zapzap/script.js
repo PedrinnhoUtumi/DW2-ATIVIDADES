dcfb41fd38322050b1c70f44fc50c2b019acdcd6
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-05-15 11:46:35 -0300

    atividade whatsapp

--- atividade zapzap/script.js
+++ atividade zapzap/script.js
@@ -1,28 +1,33 @@
 const telefoneRegex = /^(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})$/;
-const sara = document.getElementById("bt-icon")
-const sara2 = document.getElementById("bt-icon2")
-const sara3 = document.getElementById("copiar")
-
+const other_test = /\d{11}/
+const criar = document.getElementById("bt-icon")
+const enviar = document.getElementById("bt-icon2")
+const copia_div = document.getElementById("copiar")
+const number = document.getElementById('numero')
+const span_click = document.getElementById('click')
 
 function check_number() {
-    const number = document.getElementById('numero')
-    if (telefoneRegex.test(number.value)) { 
+    if (telefoneRegex.test(number.value) || other_test.test(number.value)) {
         number.classList.add('correct')
         number.classList.remove('error')
-        console.log("vc")
-    }else {
+        return true
+    } else {
         number.classList.add('error')
         number.classList.remove('correct')
-        console.log("eu")
+        return false
     }
-}   
-
-function create_link(event){
-    if (event.target === sara ){
-        sara3.style.display = "flex"
-        console.log("arrasei")
+}
+//https://wa.me${variavel}
+function create_link(event) {
+    if (event.target === criar || check_number()) {
+        copia_div.style.display = "flex"
+        span_click.style.display = "flex"
+    } else {
+        copia_div.style.display = "none"
+        span_click.style.display = "none"
     }
 }
 
-document.getElementById('bt-icon').addEventListener('click', check_number);
-document.getElementById('bt-icon2').addEventListener('click', check_number);
+number.addEventListener('input', check_number);
+criar.addEventListener('click', create_link);
+enviar.addEventListener('click', create_link);
