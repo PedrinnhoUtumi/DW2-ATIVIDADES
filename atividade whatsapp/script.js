93e4ad1b0b079c340c5ec40b34f375ffb9a8960e
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-05-17 10:48:42 -0300

    atividades

--- atividade whatsapp/script.js
+++ atividade whatsapp/script.js
@@ -1,22 +1,22 @@
 const regex = /\d{11}/
-const criar = document.getElementById("bt-icon")
-const enviar = document.getElementById("bt-icon2")
-const copia_div = document.getElementById("copiar")
+const criar = document.getElementById('bt-icon')
+const enviar = document.getElementById('bt-icon2')
+const copia_div = document.getElementById('copiar')
 const number = document.getElementById('numero')
 const span_click = document.getElementById('click')
 
 function check_number() {
-    let numeros = ""
+    let numeros = ''
     for (let n of number.value) {
         switch (n) {
             case '(':
-                numeros.replace("")
+                numeros.replace('')
             case ')':
-                numeros.replace("")
+                numeros.replace('')
             case '-':
-                numeros.replace("")
+                numeros.replace('')
             case ' ':
-                numeros.replace("")
+                numeros.replace('')
                 break;
             default:
                 numeros += n;
@@ -35,17 +35,17 @@ function check_number() {
 
 
 function create_link() {
-    let numeros = ""
+    let numeros = ''
     for (let n of number.value) {
         switch (n) {
             case '(':
-                numeros.replace("")
+                numeros.replace('')
             case ')':
-                numeros.replace("")
+                numeros.replace('')
             case '-':
-                numeros.replace("")
+                numeros.replace('')
             case ' ':
-                numeros.replace("")
+                numeros.replace('')
                 break;
             default:
                 numeros += n;
@@ -56,27 +56,32 @@ function create_link() {
     
     if (check_number()) {
         const link_copiar = `https://wa.me/${numeros}`
-        copia_div.style.display = "flex"
-        span_click.style.display = "flex"
+        copia_div.style.display = 'flex'
+        span_click.style.display = 'flex'
         copia_div.innerText = link_copiar
         let format = `(${numeros.substring(0, 2)}) ${numeros.substring(2, 7)}-${numeros.substring(7, 12)}`
         number.value = format
     } else {
-        copia_div.style.display = "none"
-        span_click.style.display = "none"
+        copia_div.style.display = 'none'
+        span_click.style.display = 'none'
     }
 }
 
-function copy(){
-    navigator.clipboard.writeText(copia_div.innerText)
-    span_click.innerText = "Texto copiado para área de transferência"    
+async function copy(){
+    try{
+        await navigator.clipboard.writeText(copia_div.innerText)
+        span_click.innerText = 'Texto copiado para área de transferência'    
+    } catch(error){
+        alert('erro', error)
+    }
 }
 
 number.addEventListener('input', check_number)
 number.addEventListener('click', () => {
-    if (span_click.style.display == "flex" || copia_div.style.display == "flex"){
-        span_click.style.display = "none"
-        copia_div.style.display = "none"
+    if (span_click.style.display == 'flex' || copia_div.style.display == 'flex'){
+        span_click.style.display = 'none'
+        span_click.innerText = 'Clique no link para copiar'
+        copia_div.style.display = 'none'
     }
 })
 criar.addEventListener('click', create_link)
@@ -87,6 +92,6 @@ enviar.addEventListener('click', () => {
     if (check_number()){
         window.open(copia_div.innerText);
     } else {
-        alert("Digite um número válido");
+        alert('Digite um número válido');
     }
 });
