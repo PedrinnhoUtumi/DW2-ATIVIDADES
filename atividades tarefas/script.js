efb06dad0b8d8b002937ec181e277c43eea16a5d
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-06-14 09:41:25 -0300

    lista de tarefa

--- atividades tarefas/script.js
+++ atividades tarefas/script.js
@@ -13,17 +13,18 @@ const tarconcluida = document.getElementById("tarconcluida")
 
 let lista = []
 let listaconcluidas = []
+
 document.addEventListener("DOMContentLoaded", function() {
     function criarDivTarefa(descricao) {
         let divTarefa = document.createElement("div")
-        divTarefa.style.cursor = "pointer"
-        /*divTarefa.addEventListener("click", () => {
-            listaconcluidas.push(descricao)
-            tarconcluida.innerText = `Tarefas concluidas ${listaconcluidas.length}`
-            lista.pop()
-            tarcriada.innerText = `Tarefas criadas ${lista.length}`
-            divTarefa.remove()
-        })*/
+        divTarefa.classList.add("divtarefa") 
+        divTarefa.innerHTML= `
+            <div id="tar">
+                <input type="checkbox" id="check" name="check"> ${descricao}            
+            </div>
+        `
+        let check = document.getElementById("check")
+        console.log(check)
         let lixo = document.createElement('img')
         lixo.src = "hover=false, type=delete.png"
         lixo.style.cursor = "pointer"
@@ -34,38 +35,29 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log("sdadadasdsadsa")
         })
         img.remove()
-        divTarefa.textContent = descricao
         divTarefa.appendChild(lixo)
-        divTarefa.style.width = "100%"
-        divTarefa.style.height = "10vh"
-        divTarefa.style.marginBottom = "5%"
-        divTarefa.style.borderRadius = "10px"
-        divTarefa.style.display = "flex"
-        divTarefa.style.alignItems = "center"
-        divTarefa.style.justifyContent = "space-around"
-        divTarefa.style.backgroundColor = "var(--cor-cinza)"
-        divTarefa.style.color = "var(--cor-branco)"
         return divTarefa
     }
 
     function adicionarTarefa(descricao) {
-        var divTarefa = criarDivTarefa(descricao);
-        containerTarefas.appendChild(divTarefa);
+        var divTarefa = criarDivTarefa(descricao)
+        containerTarefas.appendChild(divTarefa)
     }
 
     botao.addEventListener("click", function(event) {
         event.preventDefault()
         let descricao = input.value.trim() 
         if (descricao !== "") {
-            adicionarTarefa(descricao);
+            adicionarTarefa(descricao)
             lista.push(descricao)
             tarcriada.innerText = `Tarefas criadas ${lista.length}`
         }
         input.value = ""
-    });
+    })
+
     document.addEventListener("click", function(event){
         if (lista.length == 0) {
             containerTarefas.appendChild(img)
         }
     })
-});
+})
