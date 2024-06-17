fb9b936c8288e5061da7234648de2ffad8e3ed55
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-06-17 01:08:37 -0300

    Atividade Tarefas

--- atividades tarefas/script.js
+++ atividades tarefas/script.js
@@ -1,9 +1,3 @@
-/*
-1: Fazer o js criar as div;
-2: Transformar o que ta no input em objetos do JSON
-3: Colocar eles na div criada
-*/
-
 const input = document.getElementById("tarefa");
 const botao = document.getElementById("criar");
 const containerTarefas = document.getElementById("containerTarefas");
@@ -13,51 +7,79 @@ const tarconcluida = document.getElementById("tarconcluida")
 
 let lista = []
 let listaconcluidas = []
+let object = {}
 
-document.addEventListener("DOMContentLoaded", function() {
-    function criarDivTarefa(descricao) {
-        let divTarefa = document.createElement("div")
-        divTarefa.classList.add("divtarefa") 
-        divTarefa.innerHTML= `
+function criarDivTarefa(descricao) {
+    let divTarefa = document.createElement("div")
+    divTarefa.classList.add("divtarefa")
+    divTarefa.innerHTML = `
             <div id="tar">
                 <input type="checkbox" id="check" name="check"> ${descricao}            
             </div>
         `
-        let check = document.querySelector("#check")
-        console.log(check)
-        let lixo = document.createElement('img')
-        lixo.src = "hover=false, type=delete.png"
-        lixo.style.cursor = "pointer"
-        lixo.addEventListener("click", function(event){
+    let check = divTarefa.querySelector("#check")
+    console.log(check)
+    check.addEventListener("change", function () {
+        if (check.checked) {
+            listaconcluidas.push(descricao)
+            lista.pop()
+            divTarefa.style.textDecoration = "line-through"
+            tarconcluida.innerText = `Tarefas concluídas ${listaconcluidas.length}`
+            tarcriada.innerText = `Tarefas criadas ${lista.length}`
+        } else {
+            lista.push(descricao)
+            console.log(lista.length)
+            listaconcluidas.pop()
+            divTarefa.style.textDecoration = "none"
+            tarconcluida.innerText = `Tarefas concluídas ${listaconcluidas.length}`
+            tarcriada.innerText = `Tarefas criadas ${lista.length}`
+        }
+    });
+    let lixo = document.createElement('img')
+    lixo.src = "hover=false, type=delete.png"
+    lixo.style.cursor = "pointer"
+    lixo.addEventListener("click", function (event) {
+        if (check.checked) {
+            divTarefa.remove()
+            listaconcluidas.pop()
+            tarconcluida.innerText = `Tarefas concluídas ${listaconcluidas.length}`
+            console.log("sdadadasdsadsa")
+            verificarImagem()
+        } else {
             divTarefa.remove()
             lista.pop()
             tarcriada.innerText = `Tarefas criadas ${lista.length}`
             console.log("sdadadasdsadsa")
-        })
-        img.remove()
-        divTarefa.appendChild(lixo)
-        return divTarefa
-    }
-
-    function adicionarTarefa(descricao) {
-        var divTarefa = criarDivTarefa(descricao)
-        containerTarefas.appendChild(divTarefa)
-    }
-
-    botao.addEventListener("click", function(event) {
-        event.preventDefault()
-        let descricao = input.value.trim() 
-        if (descricao !== "") {
-            adicionarTarefa(descricao)
-            lista.push(descricao)
-            tarcriada.innerText = `Tarefas criadas ${lista.length}`
-        }
-        input.value = ""
-    })
-
-    document.addEventListener("click", function(event){
-        if (lista.length == 0) {
-            containerTarefas.appendChild(img)
+            verificarImagem()
         }
     })
+    img.remove()
+    divTarefa.appendChild(lixo)
+    return divTarefa
+}
+
+function adicionarTarefa(descricao) {
+    var divTarefa = criarDivTarefa(descricao)
+    containerTarefas.appendChild(divTarefa)
+}
+
+botao.addEventListener("click", function (event) {
+    event.preventDefault()
+    let descricao = input.value.trim()
+    if (descricao !== "") {
+        adicionarTarefa(descricao)
+        lista.push(descricao)
+        tarcriada.innerText = `Tarefas criadas ${lista.length}`
+    }
+    input.value = ""
 })
+
+function verificarImagem() {
+    if (lista.length === 0 && listaconcluidas.length === 0) {
+        containerTarefas.appendChild(img);
+    } else {
+        if (img.parentNode) {
+            img.parentNode.removeChild(img);
+        }
+    }
+}
